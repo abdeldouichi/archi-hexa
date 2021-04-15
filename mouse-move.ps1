568017cc2b5141c979c95224dea6487e43ffd209
abdel.douichi <abdel.douichi@gmail.com>
2021-04-15 10:50:07 +0200

    Update mouse-move.ps1

--- mouse-move.ps1
+++ mouse-move.ps1
@@ -1,10 +1,45 @@
-Add-Type -AssemblyName System.Windows.Forms
+#
+# -- NoSleep --
+# Keep your computer awake by programmatically pressing the ScrollLock key every X seconds
+#
 
-while ($true)
+param($sleep = 240) # seconds
+$announcementInterval = 10 # loops
+
+Clear-Host
+
+$WShell = New-Object -com "Wscript.Shell"
+$date = Get-Date -Format "dddd MM/dd HH:mm (K)"
+
+$stopwatch
+# Some environments don't support invocation of this method.
+try {
+    $stopwatch = [system.diagnostics.stopwatch]::StartNew()
+} catch {
+   Write-Host "Couldn't start the stopwatch."
+}
+
+Write-Host "Executing ScrollLock-toggle NoSleep routine."
+Write-Host "Start time:" $(Get-Date -Format "dddd MM/dd HH:mm (K)")
+
+Write-Host "<3" -fore red
+
+$index = 0
+while ( $true )
 {
-  $Pos = [System.Windows.Forms.Cursor]::Position
-  $x = ($pos.X % 500) + 1
-  $y = ($pos.Y % 500) + 1
-  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
-  Start-Sleep -Seconds 10
+    Write-Host "< 3" -fore red      # heartbeat
+    $WShell.sendkeys("{SCROLLLOCK}")
+
+    Start-Sleep -Milliseconds 200
+
+    $WShell.sendkeys("{SCROLLLOCK}")
+    Write-Host "<3" -fore red       # heartbeat
+
+    Start-Sleep -Seconds $sleep
+
+    # Announce runtime on an interval
+    if ( $stopwatch.IsRunning -and (++$index % $announcementInterval) -eq 0 )
+    {
+        Write-Host "Elapsed time: " $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
+    }
 }
